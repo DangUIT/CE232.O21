60bf6ee8c74dc1c2e0cd6a445dd7e57d580b57d8
trandang <trandanganninh1@gmail.com>
2024-04-02 23:13:41 +0700

    I2C

---
+++ I2C/main/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "driver/i2c.h"
+#include "sdkconfig.h"
+#include "SSD1306.h"
+
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define DATA_LENGTH 512                  /*!< Data buffer length of test buffer */
+#define RW_TEST_LENGTH 128               /*!< Data length for r/w test, [0,DATA_LENGTH] */
+#define DELAY_TIME_BETWEEN_ITEMS_MS 1000 /*!< delay time between different test items */
+
+#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(0) /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+#define SSD1306_ADDR OLED_I2C_ADDRESS   /*!< slave address for BH1750 sensor */
+
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+
+
+/**
+ * @brief i2c master initialization
+ */
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+    if (err != ESP_OK) {
+        return err;
+    }
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+void app_main(void)
+{   
+    ESP_ERROR_CHECK(i2c_master_init());                             //Innit master i2c
+    ssd1306_init();                                                 //Init ssd1306
+    task_ssd1306_display_clear();                                   //clear screen oled
+    task_ssd1306_display_text("21520684\n21520697\n21520763\n");    //display id student
+    vTaskDelay(5000/portTICK_PERIOD_MS);                            //delay 5s
+    task_ssd1306_display_clear();                                   //clear screen oled
+    drawlogo();                                                     //drawUITlogo
+}
